65495cf490d148d75f0d34bbab6184f069335839
HenriqueMorato <riquemorato@gmail.com>
2017-11-09 14:56:51 -0200

    show full recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,9 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
+  def show; end
+
+  private
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
