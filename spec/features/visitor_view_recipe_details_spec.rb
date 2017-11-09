c8f19582e3f4bfa7d3f564abe2033096ea497bfa
HenriqueMorato <riquemorato@gmail.com>
2017-11-09 17:12:02 -0200

    model changes

--- spec/features/visitor_view_recipe_details_spec.rb
+++ spec/features/visitor_view_recipe_details_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 feature 'Visitor view recipe details' do
   scenario 'successfully' do
+    #cria os dados necessários
+    cuisine = Cuisine.create(name: 'Brasileira')
     recipe = Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
-                           cuisine: 'Brasileira', difficulty: 'Médio',
-                           cook_time: 60,
-                           ingredients: 'Farinha, açucar, cenoura',
-                           method: 'Cozinhe a cenoura, corte em pedaços '\
-                                   'pequenos, misture com o restante '\
-                                   'dos ingredientes')
+                          cuisine: cuisine, difficulty: 'Médio',
+                          cook_time: 60,
+                          ingredients: 'Farinha, açucar, cenoura',
+                          method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
+    # simula a ação do usuário
     visit root_path
     click_on recipe.title
 
+    # expectativas do usuário após a ação
     expect(page).to have_css('h1', text: recipe.title)
     expect(page).to have_css('h3', text: 'Detalhes')
     expect(page).to have_css('p', text: recipe.recipe_type)
-    expect(page).to have_css('p', text: recipe.cuisine)
+    expect(page).to have_css('p', text: recipe.cuisine.name)
     expect(page).to have_css('p', text: recipe.difficulty)
     expect(page).to have_css('p', text: "#{recipe.cook_time} minutos")
     expect(page).to have_css('h3', text: 'Ingredientes')
@@ -26,18 +28,20 @@ feature 'Visitor view recipe details' do
   end
 
   scenario 'and return to recipe list' do
+    #cria os dados necessários
+    cuisine = Cuisine.create(name: 'Brasileira')
     recipe = Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
-                           cuisine: 'Brasileira', difficulty: 'Médio',
-                           cook_time: 60,
-                           ingredients: 'Farinha, açucar, cenoura',
-                           method: 'Cozinhe a cenoura, corte em pedaços '\
-                                   'pequenos, misture com o restante '\
-                                   'dos ingredientes')
+                          cuisine: cuisine, difficulty: 'Médio',
+                          cook_time: 60,
+                          ingredients: 'Farinha, açucar, cenoura',
+                          method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
+    # simula a ação do usuário
     visit root_path
     click_on recipe.title
     click_on 'Voltar'
 
+    # expectativa da rota atual
     expect(current_path).to eq(root_path)
   end
 end
