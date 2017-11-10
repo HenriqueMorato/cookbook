6c800e96f22f6c231f8d39d76eb49079466f4db0
HenriqueMorato <riquemorato@gmail.com>
2017-11-10 16:57:59 -0200

    back buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  /before_action :save_my_previous_url
+
+  def save_my_previous_url
+    session[:my_previous_url] = URI(request.referer || '').path
+    @back_url = session[:my_previous_url]
+  end/
 end
